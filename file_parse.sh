acdb9957bf205224a73741797515a7e3f391a2dd
pierre-antoine-dumond <pierre-antoine.dumond@laplateforme.io>
2022-10-07 15:56:44 +0200

    first commit

---
+++ file_parse.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+IFS=','
+while IFS=$',' read -r sentence
+do
+    echo $sentence && sleep 1
+done 
+
+apt install -y sudo 
+
+addgroup nogroup
+
+sed 1d Shell_userlist.csv | cat while read line
+
+do 
+    p1=$(echo "$line" | cut -f 1)
+    p2=$(echo "$line" | cut -f 1)
+    p3=$(echo "$line" | cut -f 1)
+    p4=$(echo "$line" | cut -f 1)
+    p5=$(echo "$line" | cut -f 1)
+done
+
---
+++ remove_proftpd.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt-get remove proftpd-core
+sudo apt-get autoremove proftpd-core
+sudo apt-get purge proftpd-core
+sudo apt-get autoremove --purge proftpd-core
+
+
